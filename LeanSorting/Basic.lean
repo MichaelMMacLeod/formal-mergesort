04b7ec4ace87dc9890764d49502d61c1be7aa2a9
Michael MacLeod <michaelmmacleod@gmail.com>
2024-06-18 21:36:33 -0700

    Almost finish proof?

--- LeanSorting/Basic.lean
+++ LeanSorting/Basic.lean
@@ -46,6 +46,8 @@ partial def mergeAdjacentChunksIntoAux [Inhabited α] [Ord α] (arr : Array α) 
       (arr_size_eq_aux_size : arr.size = aux.size)
       (i_def : i = k₁ + k₂ - start₂)
       (k₂_ge_start₂ : k₂ ≥ start₂)
+      (k₁_lt_start₂_succ : k₁ < start₂.succ)
+      (k₂_lt_end₂_succ : k₂ < end₂.succ)
       : Array α :=
     if k₁_k₂_in_bounds : k₁ < start₂ ∧ k₂ < end₂ then
       -- Without the following two proofs, the proofs required for arr[k₁] and arr[k₂] are
@@ -69,10 +71,13 @@ partial def mergeAdjacentChunksIntoAux [Inhabited α] [Ord α] (arr : Array α) 
           exact arr_size_eq_aux_size
         have i_succ_def : i.succ = k₁.succ + k₂ - start₂ := by
           omega
+        have k₁_succ_lt_start₂_succ : k₁.succ < start₂.succ := by omega
         loop aux' i.succ k₁.succ k₂
           arr_size_eq_aux'_size
           i_succ_def
           k₂_ge_start₂
+          k₁_succ_lt_start₂_succ
+          k₂_lt_end₂_succ
       | .gt =>
         let aux' := (dbgTraceIfShared "mergeChunks2" aux).set ⟨i, i_lt_aux_size⟩ arr[k₂]
         have arr_size_eq_aux'_size : arr.size = aux'.size := by
@@ -81,244 +86,78 @@ partial def mergeAdjacentChunksIntoAux [Inhabited α] [Ord α] (arr : Array α) 
           exact arr_size_eq_aux_size
         have i_succ_def : i.succ = k₁ + k₂.succ - start₂ := by omega
         have k₂_succ_ge_start₂ : k₂.succ ≥ start₂ := by omega
+        have k₂_succ_lt_end₂_succ : k₂.succ < end₂.succ := by omega
         loop aux' i.succ k₁ k₂.succ
           arr_size_eq_aux'_size
           i_succ_def
           k₂_succ_ge_start₂
+          k₁_lt_start₂_succ
+          k₂_succ_lt_end₂_succ
     else
-      have : k₁ < start₂.succ := by sorry
-      have : k₂ < end₂.succ := by sorry
-      have : ¬k₁ < start₂ → k₁ = start₂ := by omega
-      have : ¬k₂ < end₂ → k₂ = end₂ := by omega
-      have : k₁ < start₂ → k₂ = end₂ := by omega
-      have : k₂ < end₂ → k₁ = start₂ := by omega
+      have : ¬k₁ < start₂ → k₂ < end₂ := by sorry
       if k₁_lt_start₂ : k₁ < start₂ then
-        have : k₂ = end₂ := by
-          omega
         have i_lt_aux_size : i < aux.size := by omega
         let aux' := (dbgTraceIfShared "mergeChunks" aux).set ⟨i, i_lt_aux_size⟩ arr[k₁]
-        sorry
+        have arr_size_eq_aux'_size : arr.size = aux'.size := by
+          have aux'_def : aux' = aux.set ⟨i, i_lt_aux_size⟩ arr[k₁] := by rfl
+          rw [aux'_def, Array.size_set]
+          exact arr_size_eq_aux_size
+        have i_succ_def : i.succ = k₁.succ + k₂ - start₂ := by omega
+        let rec loop₁ (aux : Array α) (i : ℕ) (k₁ : ℕ)
+            (arr_size_eq_aux_size : arr.size = aux.size)
+            (i_def : i = k₁ + k₂ - start₂)
+            : Array α :=
+          if k₁_lt_start₂ : k₁ < start₂ then
+            have : k₂ = end₂ := by omega
+            have i_lt_aux_size : i < aux.size := by omega
+            let aux' := (dbgTraceIfShared "mergeChunks" aux).set ⟨i, i_lt_aux_size⟩ arr[k₁]
+            have arr_size_eq_aux'_size : arr.size = aux'.size := by
+              have aux'_def : aux' = aux.set ⟨i, i_lt_aux_size⟩ arr[k₁] := by rfl
+              rw [aux'_def, Array.size_set]
+              exact arr_size_eq_aux_size
+            have i_succ_def : i.succ = k₁.succ + k₂ - start₂ := by omega
+            loop₁ aux' i.succ k₁.succ
+              arr_size_eq_aux'_size
+              i_succ_def
+          else
+            aux
+        loop₁ aux' i.succ k₁.succ
+          arr_size_eq_aux'_size
+          i_succ_def
       else
-        sorry
-      -- have k₂_le_end₂ : k₂ ≤ end₂ := by omega
-      -- have i_le_k₂ : i ≤ k₂ := by omega
-      -- Finish copying everything from the left chunk (if there was anything left to copy).
-      -- let rec loop₁ (aux : Array α) (i : ℕ) (k₁' : ℕ)
-      --     (arr_size_eq_aux_size : arr.size = aux.size)
-      --     (i_def : i = k₁' + k₂ - start₂)
-      --     (tmp4 : i > 0 → k₁ < start₂)
-      --     -- (tmp3 : k₁' = k₁.succ)
-      --     -- (tmp1 : i = 0 → k₁ = k₁')
-      --     -- (tmp2 : i > 0 → k₁ < k₁')
-      --     : Array α :=
-      --   if k₁'_in_bounds : k₁' < start₂ then
-      --     -- have k₁_lt_start₂ : k₁ < start₂ := by
-      --     --   omega
-      --     have k₂_eq_end₂ : k₂ = end₂ := by
-      --       apply idk1 at one_k_finished
-      --       apply And.right at one_k_finished
-      --       exact one_k_finished
-      --       exact k₁_lt_start₂
-      --     have i_lt_aux_size : i < aux.size := by
-      --       omega
-      --     have k₁'_lt_arr_size : k₁' < arr.size := by omega
-      --     let aux' := (dbgTraceIfShared "mergeChunks" aux).set ⟨i, i_lt_aux_size⟩ arr[k₁']
-      --     have arr_size_eq_aux'_size : arr.size = aux'.size := by
-      --       have aux'_def : aux' = aux.set ⟨i, i_lt_aux_size⟩ arr[k₁'] := by rfl
-      --       rw [aux'_def, Array.size_set]
-      --       exact arr_size_eq_aux_size
-      --     -- have tmp' : i.succ > 0 → k₁ < k₁'.succ := by omega
-      --     loop₁ aux' i.succ k₁'.succ
-      --       arr_size_eq_aux'_size
-      --       sorry
-      --       sorry
-      --       -- (by omega)
-      --       -- (by simp[*])
-      --       -- tmp'
-      --       -- (by simp[*])
-      --   else
-      --     -- Finish copying everything from the right chunk (if there was anything left to copy).
-      --     let rec loop₂ (aux : Array α) (i : ℕ) (k₂ : ℕ)
-      --         (arr_size_eq_aux_size : arr.size = aux.size)
-      --         (i_def : i = k₁ + k₂ - start₂)
-      --         : Array α :=
-      --       if k₂_in_bounds : k₂ < end₂ then
-      --         have k₁_eq_start₂ : k₁ = start₂ := by sorry
-      --         have i_lt_aux_size : i < aux.size := by
-      --           omega
-      --         let aux' := (dbgTraceIfShared "mergeChunks" aux).set ⟨i, i_lt_aux_size⟩ arr[k₂]
-      --         loop₂ aux' i.succ k₂.succ
-      --           sorry
-      --           sorry
-      --       else
-      --         aux
-      --     loop₂ aux i k₂
-      --       sorry
-      --       sorry
-      -- loop₁ aux i k₁
-      --   sorry
-      --   sorry
-      --   -- (by omega)
-      --   -- (by omega)
-      --   -- (by
-      --   --   sorry)
-      --   -- (by
-
-      --   --   sorry)
-      --   -- -- (by omega)
-      --   -- (by omega)
+        have i_lt_aux_size : i < aux.size := by omega
+        let aux' := (dbgTraceIfShared "mergeChunks" aux).set ⟨i, i_lt_aux_size⟩ arr[k₂]
+        have arr_size_eq_aux'_size : arr.size = aux'.size := by
+          have aux'_def : aux' = aux.set ⟨i, i_lt_aux_size⟩ arr[k₂] := by rfl
+          rw [aux'_def, Array.size_set]
+          exact arr_size_eq_aux_size
+        have i_succ_def : i.succ = k₁ + k₂.succ - start₂ := by omega
+        let rec loop₂ (aux : Array α) (i : ℕ) (k₂ : ℕ)
+            (arr_size_eq_aux_size : arr.size = aux.size)
+            (i_def : i = k₁ + k₂ - start₂)
+            : Array α :=
+          if k₂_lt_end₂ : k₂ < end₂ then
+            have i_lt_aux_size : i < aux.size := by omega
+            let aux' := (dbgTraceIfShared "mergeChunks" aux).set ⟨i, i_lt_aux_size⟩ arr[k₂]
+            have arr_size_eq_aux'_size : arr.size = aux'.size := by
+              have aux'_def : aux' = aux.set ⟨i, i_lt_aux_size⟩ arr[k₂] := by rfl
+              rw [aux'_def, Array.size_set]
+              exact arr_size_eq_aux_size
+            have i_succ_def : i.succ = k₁ + k₂.succ - start₂ := by omega
+            loop₂ aux' i.succ k₂.succ
+              arr_size_eq_aux'_size
+              i_succ_def
+          else
+            aux
+        loop₂ aux' i.succ k₂.succ
+          arr_size_eq_aux'_size
+          i_succ_def
   loop aux start₁ start₁ start₂
-    sorry
-    sorry
-    sorry
-    -- (by simp[*])
-    -- (by simp[])
-    -- (by simp[])
-
-/--
-Merges two ordered contiguous portions of `arr` into `aux`, returning `aux`.
-If there are no other references to `aux`, it will be mutated in-place.
-
-For example,
-```
-                        region1    region2
-arr: 1 2 3 100 200 300 [10 20 30 │ 21 22 23] 400 500 600
-aux: 1 2 3 100 200 300 [0  0  0  │ 0  0  0 ] 0   0   0
----> 1 2 3 100 200 300 [10 20 21 │ 22 23 30] 0   0   0
-                        │          │         │
-chunkStart1 ────────────┘          │         │
-chunkStart2 ───────────────────────┘         │
-end2 ────────────────────────────────────────┘
-```
--/
--- partial def mergeAdjacentChunksIntoAux2 [Inhabited α] [Ord α] (arr : Array α) (aux : Array α)
---     (chunkStart₁ : ℕ) (chunkStart₂ : ℕ) (chunkEnd₂ : ℕ)
---     (start₁_lt_start₂ : chunkStart₁ < chunkStart₂)
---     (start₂_lt_end₂ : chunkStart₂ < chunkEnd₂)
---     (end₂_le_arr_size : chunkEnd₂ ≤ arr.size)
---     (arr_size_eq_aux_size : arr.size = aux.size)
---     : Array α :=
---   -- Copy from both the left and right chunk until one of the chunks is fully copied.
---   let rec loop (aux : Array α) (i : ℕ) (k₁ : ℕ) (k₂ : ℕ)
---       (k₁_ge_chunkStart₁ : k₁ ≥ chunkStart₁)
---       (arr_size_eq_aux_size : arr.size = aux.size)
---       (i_in_single_chunk :
---         Xor' (i ≥ chunkStart₁ ∧ i < chunkStart₂)
---              (i ≥ chunkStart₂ ∧ i < chunkEnd₂))
---       : Array α :=
---     if k₁_k₂_in_bounds : k₁ < chunkStart₂ ∧ k₂ < chunkEnd₂ then
-
---       -- Without the following two proofs, the proofs required for arr[k₁] and arr[k₂] are
---       -- automatically inferred (via `omega`?), which for whatever reason messes with the
---       -- reference counter for `aux`, resulting in a full copy when we do `aux.set`.
---       have k₁_lt_arr_size : k₁ < arr.size := by
---         omega
---         -- apply And.left at k₁_k₂_in_bounds
---         -- let start₂_lt_arr_size := Nat.lt_trans start₂_lt_end₂ end₂_lt_arr_size
---         -- exact (Nat.lt_trans k₁_k₂_in_bounds start₂_lt_arr_size)
---       have k₂_lt_arr_size : k₂ < arr.size := by
---         omega
---         -- apply And.right at k₁_k₂_in_bounds
---         -- exact (Nat.lt_trans k₁_k₂_in_bounds end₂_lt_arr_size)
-
---       have i_lt_aux_size : i < aux.size := by
---         rw [Xor'] at i_in_single_chunk
---         omega
-
---       match Ord.compare arr[k₁] arr[k₂] with
---       | .lt | .eq =>
---         let aux' := (dbgTraceIfShared "mergeChunks1" aux).set ⟨i, i_lt_aux_size⟩ arr[k₁]
---         have arr_size_eq_aux'_size : arr.size = aux'.size := by
---           have aux'_def : aux' = aux.set ⟨i, i_lt_aux_size⟩ arr[k₁] := by rfl
---           rw [aux'_def]
---           rw [Array.size_set]
---           exact arr_size_eq_aux_size
---         have k₁_succ_ge_chunkStart₁ : k₁.succ ≥ chunkStart₁ := by
---           exact (Nat.succ_ge_of_ge k₁_ge_chunkStart₁)
---         have i_in_single_chunk' :
---             Xor' (i.succ ≥ chunkStart₁ ∧ i.succ < chunkStart₂)
---                  (i.succ ≥ chunkStart₂ ∧ i.succ < chunkEnd₂) := by
---           rw [Xor'] at i_in_single_chunk
---           rw [Xor']
-
---         loop aux' i.succ k₁.succ k₂
---           k₁_succ_ge_chunkStart₁
---           arr_size_eq_aux'_size
---           i_in_single_chunk'
---       | .gt =>
---         let aux' := (dbgTraceIfShared "mergeChunks2" aux).set ⟨i, i_lt_aux_size⟩ arr[k₂]
---         have arr_size_eq_aux'_size : arr.size = aux'.size := by
---           have aux'_def : aux' = aux.set ⟨i, i_lt_aux_size⟩ arr[k₂] := by rfl
---           rw [aux'_def]
---           rw [Array.size_set]
---           exact arr_size_eq_aux_size
---         have i_in_single_chunk' :
---             Xor' (i.succ ≥ chunkStart₁ ∧ i.succ < chunkStart₂)
---                  (i.succ ≥ chunkStart₂ ∧ i.succ < chunkEnd₂) :=
---           sorry
---         loop aux' i.succ k₁ k₂.succ
---           k₁_ge_chunkStart₁
---           arr_size_eq_aux'_size
---           i_in_single_chunk'
---     else
---       -- Finish copying everything from the left chunk (if there was anything left to copy).
---       let rec loop₁ (aux : Array α) (i : ℕ) (k₁ : ℕ) :=
---         if k₁ < chunkStart₂ then
---           have i_lt_aux_size : i < aux.size := sorry
---           have k₁_lt_arr_size : k₁ < arr.size := sorry
---           let aux' := (dbgTraceIfShared "mergeChunks" aux).set ⟨i, i_lt_aux_size⟩ arr[k₁]
---           loop₁ aux' i.succ k₁.succ
---         else
---           -- Finish copying everything from the right chunk (if there was anything left to copy).
---           let rec loop₂ (aux : Array α) (i : ℕ) (k₂ : ℕ) :=
---             if k₂ < chunkEnd₂ then
---               have i_lt_aux_size : i < aux.size := sorry
---               have k₂_lt_arr_size : k₂ < arr.size := sorry
---               let aux' := (dbgTraceIfShared "mergeChunks" aux).set ⟨i, i_lt_aux_size⟩ arr[k₂]
---               loop₂ aux' i.succ k₂.succ
---             else
---               aux
---           loop₂ aux i k₂
---       loop₁ aux i k₁
---   loop aux chunkStart₁ chunkStart₁ chunkStart₂
---     (by simp [*])
---     arr_size_eq_aux_size
---     (by simp [*])
-
--- def mergeAdjacentChunksIntoAux2
-      -- have k₁_lt_arr_size : k₁ < arr.size := by
-      --   -- apply And.left at k₁_k₂_in_bounds
-      --   -- have h3 := Nat.lt_trans start₂_lt_end₂ end₂_lt_arr_size
-      --   omega
-      -- have k₂_lt_arr_size : k₂ < arr.size := sorry
-
---[Inhabited α] [Ord α] (arr : Array α) (aux : Array α)
---     (chunkStart₁ : ℕ) (chunkStart₂ : ℕ) (chunkEnd₂ : ℕ) : Array α := Id.run do
---   let mut i := chunkStart₁
---   let mut aux := aux
---   let mut k₁ := chunkStart₁
---   let mut k₂ := chunkStart₂
---   while k₁ < chunkStart₂ ∧ k₂ < chunkEnd₂ do
---     match Ord.compare arr[k₁]! arr[k₂]! with
---     | .lt | .eq =>
---       aux := (dbgTraceIfShared "mergeChunks" aux).set! i arr[k₁]!
---       k₁ := k₁ + 1
---     | .gt =>
---       aux := (dbgTraceIfShared "mergeChunks" aux).set! i arr[k₂]!
---       k₂ := k₂ + 1
---     i := i + 1
---   while k₁ < chunkStart₂ do
---     aux := (dbgTraceIfShared "mergeChunks" aux).set! i arr[k₁]!
---     k₁ := k₁ + 1
---     i := i + 1
---   while k₂ < chunkEnd₂ do
---     aux := (dbgTraceIfShared "mergeChunks" aux).set! i arr[k₂]!
---     k₂ := k₂ + 1
---     i := i + 1
---   while i < aux.size do
---     aux := (dbgTraceIfShared "mergeChunks" aux).set! i arr[i]!
---     i := i + 1
---   pure aux
+    (by omega)
+    (by omega)
+    (by omega)
+    (by omega)
+    (by omega)
 
 def mergeChunksIntoAux [Inhabited α] [Ord α] (arr : Array α) (aux : Array α)
     (chunkSize : ℕ) : Array α := Id.run do
@@ -331,7 +170,9 @@ def mergeChunksIntoAux [Inhabited α] [Ord α] (arr : Array α) (aux : Array α)
                 arr aux chunkStart₁ chunkStart₂ chunkEnd₂
                 (by sorry) (by sorry) (by sorry) (by sorry)
     chunkStart₁ := chunkStart₁ + 2 * chunkSize
-
+  while chunkStart₁ < arr.size do
+    aux := (dbgTraceIfShared "mergeChunksIntoAux" aux).set! chunkStart₁ arr[chunkStart₁]!
+    chunkStart₁ := chunkStart₁ + 1
   pure aux
 
 def Array.overwrite_with (dest : Array α) (src : Array α) (hp : src.size = dest.size) : Array α :=
@@ -361,4 +202,4 @@ def Array.mergeSort [Inhabited α] [Ord α] (arr : Array α) : Array α := Id.ru
   pure arr
 
 #eval mergeChunksIntoAux #[3, 2, 1] #[0, 0, 0] 1
-#eval #[3, 2, 1].mergeSort
+#eval #[8, 7, 6, 5, 4, 3, 2, 1].mergeSort
