0438e0907ec0c06d916480838ac849830299d193
Michael MacLeod <michaelmmacleod@gmail.com>
2024-07-17 15:13:00 -0700

    Add profiling results

---
+++ profile.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+profile() {
+    echo "Testing $1"
+    for _ in {1..10}; do
+        { time ./.lake/build/bin/lsort --"$1" < ./test-input/1-million-lines.txt > /dev/null ; } 2>&1 \
+            | head -2 \
+            | tail -1
+    done
+}
+
+profile mergeSortPartial
+profile mergeSort
+profile qsort
+profile heapSort
